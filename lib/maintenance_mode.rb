fe192e585c137748962b52fbe49eff6d3617274b
bogdancojan <Bogdan.Cojan@theaccessgroup.com>
2024-09-20 01:17:13 +0300

    Bump version to 0.1.1

--- lib/maintenance_mode.rb
+++ lib/maintenance_mode.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "maintenance_mode/version"
+require_relative "maintenance_mode/middleware"
 
 module MaintenanceMode
   class Error < StandardError; end
